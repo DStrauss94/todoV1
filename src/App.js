701d7ad41c7bcff9e8c04a896b732a621c7d47ad
Douglas Strauss <douglasstrauss94@gmail.com>
2021-03-18 21:25:09 -0700

    3.18.2021

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from "./components/Form";
 import TodoList from './components/TodoList';
 
 function App() {
-  const [inputText, setInputText] = useState('');
+  const [inputText , setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
-        <h1>Doogie's Todo List</h1>
+        <h1>DevDoogs Todo List</h1>
       </header>
-      <Form setInputText={setInputText}/>
-      <TodoList />
+      <Form 
+      inputText= {inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+
+      />
+      <TodoList  />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
 
-const Form = ({setInputTexts}) => {
+const Form = ({setInputText, todos, setTodos, inputText}) => {
+
     //Here I can write javascript code and functions
     const inputTextHandler = (e) => {
         console.log(e.target.value);
-        setInputTexts = e.target.vaulue;
-    }
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = (e) => {
+      e.preventDefault();
+      setTodos([
+        ...todos, { text: inputText, completed: false, id:Math.random() * 1000},
+      ]);
+    };
     return(
         <form>
-        <input onChange={inputTextHandler} type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
-          <i className="fas fa-plus-square"></i>
-        </button>
-        <div className="select">
-          <select name="todos" className="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="uncompleted">Uncompleted</option>
-          </select>
-        </div>
+          <input onChange={inputTextHandler} type="text" className="todo-input" />
+          <button onClick={submitTodoHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+          </button>
+          <div className="select">
+            <select name="todos" className="filter-todo">
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+            </select>
+          </div>
       </form>
     );
 }
